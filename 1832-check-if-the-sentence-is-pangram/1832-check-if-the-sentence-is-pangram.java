ddb275e043a3c0797906d2a9960de9fdd10bea21
Krishankant Dixit <krishankantdixit778@gmail.com>
2026-07-30 21:45:56 +0530

    Time: 4 ms (19.6%), Space: 43.1 MB (17.19%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+
+        HashSet<Character> set = new HashSet<>();
+
+        for (int i = 0; i < sentence.length(); i++) {
+            set.add(sentence.charAt(i));
+        }
+
+        return set.size() == 26;
+    }
+}
